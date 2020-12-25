20cfbe0b315e0451c84cfaf60d3f43370a89cc2b
macaba <macaba@gmail.com>
2020-12-25 22:40:47 +0000

    Initial commit

---
+++ src/SpicyReader/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading;
+
+namespace SpicyReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SpicyReader reader = new SpicyReader();
+            CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
+            reader.OpenPcapNGFile(@"C:\2222222222222.pcapng", false, cancellationTokenSource.Token);
+        }
+    }
+}
---
+++ src/SpicyReader/SpicyReader.cs
@@ -0,0 +1,70 @@
+﻿using OpenCvSharp;
+using PcapngUtils.Common;
+using PcapngUtils.PcapNG;
+using System;
+using System.Threading;
+
+namespace SpicyReader
+{
+    public class SpicyReader
+    {
+        const int width = 1024;
+        const int height = 1024;
+        bool processingFrame = false;
+        int lineCounter = 0;
+        int frameCounter = 0;
+        readonly ushort[,] integers = new ushort[height, width];
+        bool secondHalfOfLine = false;
+        //List<Mat> imageArray = new List<Mat>();
+
+        public void OpenPcapNGFile(string filename, bool swapBytes, CancellationToken token)
+        {
+            using (var reader = new PcapNGReader(filename, swapBytes))
+            {
+                reader.OnReadPacketEvent += reader_OnReadPacketEvent;
+                reader.ReadPackets(token);
+            }
+        }
+        void reader_OnReadPacketEvent(object context, IPacket packet)
+        {
+            if (packet.Data.Length == 60)
+            {
+                //Start of frame packet
+                if (processingFrame)
+                {
+                    Mat m = new Mat(width, height, MatType.CV_16U, integers);
+                    Cv2.ImWrite(frameCounter.ToString() + ".tiff", m);
+                    //imageArray.Add(m);
+                    frameCounter++;
+                }
+
+                lineCounter = 0;
+                processingFrame = true;
+            }
+            else
+            {   // Line packet.
+                if (processingFrame)
+                {
+                    int lineOffset = 0;
+                    if (secondHalfOfLine)
+                    {
+                        lineOffset += 512;
+                    }
+
+                    for (int pixel = 0; pixel < 512; pixel++)
+                    {
+                        var span = packet.Data.AsSpan().Slice(24 + (pixel * 2), 2);
+                        span.Reverse();
+                        var number = BitConverter.ToUInt16(span);
+                        number *= 20;
+                        integers[lineCounter, lineOffset + pixel] = number;
+                    }
+
+                    if(secondHalfOfLine)
+                        lineCounter++;
+                    secondHalfOfLine = !secondHalfOfLine;                   
+                }
+            }
+        }
+    }
+}
